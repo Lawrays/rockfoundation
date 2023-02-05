aa5fd36c1b5c3c2c01e9861a88c2b001c05425d1
Lawrays <119711283+Lawrays@users.noreply.github.com>
2023-02-05 03:37:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+window.onload
+let container = document.getElementById("container");
+const preview = document.querySelector(".prev");
+const active = document.querySelectorAll(".thumb");
+let btn = document.querySelector(".checkbtn")
+let check = document.getElementById("check")
+let video = document.getElementById("myvideo")
+ 
+/*btn.addEventListener("click", ()=> {
+  check.classList.toggle("show")
+});*/
+
+container.addEventListener("click", function (e) {
+  if (e.target.className == "thumb") {
+    preview.src = e.target.src;
+    preview.classList.add("effect");
+ 
+    setTimeout(function () {
+      preview.classList.remove("effect");
+    }, 100);
+ 
+    active.forEach(function (thumb) {
+      if (thumb.classList.contains("active")) {
+        thumb.classList.remove("active");
+      }
+    });
+ 
+    e.target.classList.add("active");
+  }
+});
+video.autoplay = true
+video.onload
